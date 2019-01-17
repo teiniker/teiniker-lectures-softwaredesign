32bf712dda128e92bc1ea93bcda47dc228425ce5
teiniker <egon.teiniker@gmail.com>
2019-01-17 17:12:48 +0100

    Initial commit.

---
+++ design-patterns/behavioral-patterns/Strategy-BubbleSort/src/test/java/org/se/lab/BubbleSortStrategyTest.java
@@ -0,0 +1,39 @@
+package org.se.lab;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BubbleSortStrategyTest
+{
+	protected SorterFactory factory;
+
+	@Before
+	public void setup()
+	{
+		factory = new SorterFactoryImpl();
+	}
+
+
+	@Test
+	public void testAscenting()
+	{
+		Sorter bubble = factory.createSorter(SortDirection.ASCENDING);
+
+		int[] result = bubble.sort(7, 3, 19, 123, 2, 13);
+
+		Assert.assertEquals("[2, 3, 7, 13, 19, 123]", Arrays.toString(result));
+	}
+
+	@Test
+	public void testDescending()
+	{
+		Sorter bubble = factory.createSorter(SortDirection.DESCENDING);
+
+		int[] result = bubble.sort(7, 3, 19, 123, 2, 13);
+
+		Assert.assertEquals("[123, 19, 13, 7, 3, 2]", Arrays.toString(result));
+	}
+}
