ba4ca7a5ee845a59e44dc1f4a9f274b7be35a194
teiniker <egon.teiniker@gmail.com>
2019-01-25 10:17:20 +0100

    Refactoring.

--- design-patterns/structural-patterns/Composite-FileSystem/src/test/java/org/se/lab/DirectoryTest.java
+++ design-patterns/structural-patterns/Composite-FileSystem/src/test/java/org/se/lab/DirectoryTest.java
@@ -9,12 +9,15 @@ public class DirectoryTest
 	public void setup()
 	{
 		root = new Directory("/");
+
 		Node home = new Directory("home");
 		root.addNode(home);
+
 		Node teini = new Directory("teini");
 		home.addNode(teini);
 		teini.addNode(new File(".bashrc", 1024));
 		teini.addNode(new File("todo.txt", 2048));
+
 		Node tomcat = new Directory("tomcat");
 		home.addNode(tomcat);
 		tomcat.addNode(new File(".bashrc", 512));
