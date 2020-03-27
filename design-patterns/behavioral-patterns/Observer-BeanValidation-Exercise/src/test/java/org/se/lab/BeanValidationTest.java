cd0b9b9c068cf2c2bc1d529df25cf8908f009aa5
teiniker <egon.teiniker@gmail.com>
2020-03-27 18:46:50 +0100

    Bugfix.

--- design-patterns/behavioral-patterns/Observer-BeanValidation-Exercise/src/test/java/org/se/lab/BeanValidationTest.java
+++ design-patterns/behavioral-patterns/Observer-BeanValidation-Exercise/src/test/java/org/se/lab/BeanValidationTest.java
@@ -2,49 +2,49 @@ package org.se.lab;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.se.lab.Bean;
+import org.se.lab.BeanValidationObserver;
 
 
 public class BeanValidationTest
 {
-	private Bean element;
+	private Bean bean;
 	
 	@Before
 	public void setup()
 	{
-		element = new Bean(7,"Entity");
+		bean = new Bean(7,"Entity");
 		// Register an observer which checks state changes.
-		element.add(new BeanValidationObserver());		
+		bean.addObserver(new BeanValidationObserver());		
 	}
 	
 	@Test
 	public void testModelElement()
 	{		
-		element.setName("Entity");
-		element.setId(1);
+		bean.setName("Entity");
+		bean.setId(1);
 	}
 
 	@Test(expected = IllegalStateException.class)
 	public void testNameNull()
 	{		
-		element.setName(null);		
+		bean.setName(null);		
 	}
 
 	@Test(expected = IllegalStateException.class)
 	public void testNameNullTooShort()
 	{		
-		element.setName("123");		
+		bean.setName("123");		
 	}
 	
 	@Test(expected = IllegalStateException.class)
 	public void testNameNullTooLong()
 	{		
-		element.setName("123456789012345678901234567890123");		
+		bean.setName("123456789012345678901234567890123");		
 	}
 	
 	@Test(expected = IllegalStateException.class)
 	public void testIdNegative()
 	{		
-		element.setId(-1);		
+		bean.setId(-1);		
 	}
 }
